2f9cce5b26207bf9dac6df7003bd79c813306b07
rob <gon.luna234@gmail.com>
2024-11-26 16:10:25 +0800

    semi-working

--- upload_files.bat
+++ upload_files.bat
@@ -1,61 +1,84 @@
 @echo off
-:: Set the path to WinSCP (use the full path if needed)
 set winscp=C:\Users\gonlu\AppData\Local\Programs\WinSCP\WinSCP.exe
-
-:: Define local directory and remote directory
-set localDir=C:\Users\gonlu\Desktop\testfolder\hello
-set remoteDir=/hello
-
-:: Server connection details
-set host=192.168.137.1
+set localDir=C:\Users\gonlu\Desktop\TEST
+set remoteDir=/
+set host=192.168.18.146
 set user=rob
 set password=1234
+set logFile=%localDir%\transfer_log.txt
 
-:: Generate WinSCP script for transferring files
-echo open ftp://%user%:%password%@%host% > temp_script.txt
-echo lcd "%localDir%" >> temp_script.txt
-echo cd %remoteDir% >> temp_script.txt
-echo put * >> temp_script.txt
-echo exit >> temp_script.txt
+:: Append to the log file (instead of overwriting)
+echo ------------------------------------------ >> "%logFile%"
+echo Transfer Log - %date% %time% >> "%logFile%"
+echo ------------------------------------------ >> "%logFile%"
+echo. >> "%logFile%"
 
-:: Run the script with WinSCP to transfer the files
-%winscp% /script=temp_script.txt /log=upload_log.txt /ini=nul
-set transferResult=%errorlevel%
-
-:: Check if the transfer was successful using WinSCP's exit code
-if %transferResult% equ 0 (
-    echo Transfer was successful. Attempting to delete files...
-
-    :: List files before deletion
-    echo Listing files before deletion in %localDir%:
-    dir "%localDir%" /s > before_delete_log.txt
-
-    :: Attempt to delete files recursively (but keep folders)
-    echo Attempting to delete files recursively...
-    for /R "%localDir%" %%F in (*) do (
-        if not "%%~aF"=="d" (
-            del /Q /F "%%F" >> delete_log.txt 2>&1
-        )
+:: Log files to be transferred
+echo [Files to be transferred] >> "%logFile%"
+for %%f in ("%localDir%\*") do (
+    if /i not "%%~nxf"=="transfer_log.txt" (
+        echo [INFO] %%~nxf - %date% %time% >> "%logFile%"
     )
-
-    :: List files after deletion attempt
-    echo Listing remaining files after delete attempt:
-    dir "%localDir%" /s > after_delete_log.txt
-
-    :: Check if files were actually deleted
-    if exist "%localDir%\*" (
-        echo Some files could not be deleted. Check delete_log.txt for errors.
-        echo Remaining files:
-        type after_delete_log.txt
-    ) else (
-        echo All files successfully deleted.
-    )
-) else (
-    echo Transfer failed. No files deleted. WinSCP error code: %transferResult%
 )
 
-:: Clean up
-del temp_script.txt
+:: Prepare a temporary WinSCP script file
+set scriptFile=transfer.txt
+(
+    echo open sftp://%user%:%password%@%host%
+    echo lcd %localDir%
+    echo cd %remoteDir%
+    echo put * -filemask="|transfer_log.txt"
+    echo exit
+) > %scriptFile%
 
-:: The script will close automatically after completion
-exit
+:: Execute the file transfer
+%winscp% /script=%scriptFile%
+set transferResult=%errorlevel%
+
+:: Check transfer result
+if %transferResult% equ 0 (
+    echo [SUCCESS] File transfer completed successfully - %date% %time% >> "%logFile%"
+    echo Deleting local files...
+    
+    :: Delete files in the local directory and subfolders
+    for /r "%localDir%" %%f in (*) do (
+        if /i not "%%~nxf"=="transfer_log.txt" (
+            del "%%f"
+            if exist "%%f" (
+                echo [ERROR] Failed to delete file: %%~nxf - %date% %time% >> "%logFile%"
+            ) else (
+                echo [SUCCESS] Deleted file: %%~nxf - %date% %time% >> "%logFile%"
+            )
+        )
+    )
+) else (
+    echo [ERROR] File transfer failed. Exit code: %transferResult% - %date% %time% >> "%logFile%"
+)
+
+:: Add log file upload to the script file
+(
+    echo open sftp://%user%:%password%@%host%
+    echo lcd %localDir%
+    echo cd %remoteDir%
+    echo put "%logFile%"
+    echo exit
+) > %scriptFile%
+
+:: Upload the log file to the server
+%winscp% /script=%scriptFile%
+set uploadLogResult=%errorlevel%
+
+:: Check log upload result
+if %uploadLogResult% equ 0 (
+    echo [SUCCESS] Log file uploaded to server - %date% %time% >> "%logFile%"
+) else (
+    echo [ERROR] Failed to upload log file. Exit code: %uploadLogResult% - %date% %time% >> "%logFile%"
+)
+
+:: Clean up temporary script file
+del %scriptFile%
+
+:: Optional: Clean up the local log file
+:: del "%logFile%"
+
+pause 
